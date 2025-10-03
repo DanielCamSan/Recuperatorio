b1cab41b7df0dce2ad41e1c974b94412493b5ad0
luisAguilar <luis.aguilar.c@ucb.edu.bo>
2025-10-03 16:23:59 -0400

    feat:addign Model Guest,CreateDto and UpdateDto

---
+++ Models/Guest.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Guest
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    [EmailAddress]
+    public string Email {  get; set; } = string.Empty;
+    [Range(7,20)]
+    public string Phone {  get; set; } = string.Empty;
+    public bool Confirmed { get; set; } = false;
+}
+public record CreateGuestDto
+{
+    public string FullName {  set; get; } = string.Empty;
+    [EmailAddress]
+    public string Email { get; set; } =string.Empty;
+    [Range(7, 20)]
+    public string Phone { get; set; } = string.Empty;
+    public bool Confirmed { get; set; } = false;
+}
+public record UpdateGuestDto
+{
+    public required string FullName { set; get; }
+    [EmailAddress]
+    public  required string Email { get; set; }
+    [Range(7, 20)]
+    public  required string Phone { get; set; }
+    public bool Confirmed { get; set; } = false;
+}
