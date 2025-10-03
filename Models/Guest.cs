28553567c0927b7e69b685fee138a5d4b33f14e2
Your Name <you@example.com>
2025-10-03 17:43:36 -0400

    feat(party) recuperatorio finalizado)

---
+++ Models/Guest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class Guest
+{
+    public Guid Id { get; set; }
+    [Required, StringLength(100)]
+    public string FullName { get; set; }=string.Empty;
+    [Required, StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+    [Required, StringLength(100)]
+    public string PhoneNumber { get; set; } = string.Empty;
+    public bool Confirmed { get; set; }
+}
+
+public record CreateUserDto
+{
+    [Required, StringLength(100)]
+    public string FullName { get; set; } = string.Empty;
+    [Required, StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+    [Required, StringLength(100)]
+    public string PhoneNumber { get; set; } = string.Empty;
+    public bool Confirmed { get; set; }
+}
+
+public record UpdateUserDto
+{
+    [Required, StringLength(100)]
+    public string FullName { get; set; } = string.Empty;
+    [Required, StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+    [Required, StringLength(100)]
+    public string PhoneNumber { get; set; } = string.Empty;
+    public bool Confirmed { get; set; }
+}
