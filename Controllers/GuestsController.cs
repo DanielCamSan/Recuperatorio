8b2b82f02bb551624f130949f1518a9fa4fba4d9
luisAguilar <luis.aguilar.c@ucb.edu.bo>
2025-10-03 17:19:33 -0400

    feat:adding getOne

---
+++ Controllers/GuestsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+namespace Recuperatorio.Controllers
+{
+    [ApiController]
+    [Route("api/v1/{controller}")]
+    public class GuestsController : ControllerBase
+    {
+        private static readonly List<Guest> _guests = new()
+        {
+            new Guest {Id = Guid.NewGuid(),FullName="LuisAguilar",Email="a@gmail.com",Phone="11111111",Confirmed=true},
+            new Guest {Id = Guid.NewGuid(),FullName="Leo",Email="b@gmail.com",Phone="22222222",Confirmed=true},
+            new Guest {Id = Guid.NewGuid(),FullName="Nicole",Email="c@gmail.com",Phone="33333333",Confirmed=false},
+            new Guest {Id = Guid.NewGuid(),FullName="Carol",Email="d@gmail.com",Phone="44444444",Confirmed=true},
+            new Guest {Id = Guid.NewGuid(),FullName="Adrian",Email="e@gmail.com",Phone="55555555",Confirmed=false},
+        };
+        [HttpGet("{id:guid}")]
+        public IActionResult GetOne(Guid id)
+        {
+            var guest = _guests.FirstOrDefault(g => g.Id == id);
+            return guest is null ? NotFound(new { error = "Guest not found", status = 404 })
+                : Ok(guest);
+        }
+        
+    }
+}
--- Models/Guest.cs
+++ Models/Guest.cs
@@ -15,7 +15,7 @@ public record CreateGuestDto
     public string FullName {  set; get; } = string.Empty;
     [EmailAddress]
     public string Email { get; set; } =string.Empty;
-    [Range(7, 20)]
+    
     public string Phone { get; set; } = string.Empty;
     public bool Confirmed { get; set; } = false;
 }
@@ -24,7 +24,7 @@ public record UpdateGuestDto
     public required string FullName { set; get; }
     [EmailAddress]
     public  required string Email { get; set; }
-    [Range(7, 20)]
+    
     public  required string Phone { get; set; }
     public bool Confirmed { get; set; } = false;
 }
