630749058aad7dda8c1e9aea5b4f79aa1db7aaa9
Josue-Camacho <josuecamacho582@gmail.com>
2025-10-03 16:40:50 -0400

    feat: add crud GetOne

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -1,6 +1,29 @@
-﻿namespace Recuperatorio.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Recuperatorio.Controllers
 {
-    public class TicketsController
+
+    [ApiController]
+    [Route("api/{controller}")]
+    public class TicketsController:ControllerBase
     {
+        private static readonly List<Ticket> _tickets = new()
+        {
+            new Ticket{Id=Guid.NewGuid(),GuestId=Guid.NewGuid(),EventId=Guid.NewGuid(),Type="general",Price= 10,Status="valid",Notes="sfnskjgn"},
+            new Ticket{Id=Guid.NewGuid(),GuestId=Guid.NewGuid(),EventId=Guid.NewGuid(),Type="general",Price= 10,Status="valid",Notes="sfnskjgn"},
+            new Ticket{Id=Guid.NewGuid(),GuestId=Guid.NewGuid(),EventId=Guid.NewGuid(),Type="vip",Price= 100,Status="valid",Notes="sfnskjgn"},
+            new Ticket{Id=Guid.NewGuid(),GuestId=Guid.NewGuid(),EventId=Guid.NewGuid(),Type="backstage",Price= 50,Status="valid",Notes="sfnskjgn"},
+        };
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetOne(Guid id)
+        {
+            var ticket = _tickets.FirstOrDefault(b => b.Id == id);
+            return ticket is null
+                ? NotFound(new { error = "Ticket not found", status = 404 })
+                : Ok(ticket);
+        }
+       
     }
 }
