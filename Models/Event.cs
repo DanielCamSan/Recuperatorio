f4307954c8adff9a7a765ac0e632c33af102921f
PelucheW <clonesomg@gmail.com>
2025-10-03 16:24:10 -0400

    feat: events models, CreateDto, UpdateDto

---
+++ Models/Event.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+public class Event
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public string Theme { get; set; } = string.Empty;
+    public int Capacity { get; set; }
+}
+
+public record CreateEventDto
+{
+    public string Title { get; set;} = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public string Theme { get; set; } = string.Empty;
+    public int Capacity { get; set; }
+
+
+}
+
+public record UpdateEventDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public string Theme { get; set; } = string.Empty;
+    public int Capacity { get; set; }
+
+}
