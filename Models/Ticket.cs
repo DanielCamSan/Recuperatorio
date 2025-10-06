349f01b7dcd61c85ec3f1db65029f1167ea5bf82
unknown <ernes777rim@gmail.com>
2025-10-06 17:06:14 -0400

    Feat: Added Tickets DTO

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -19,4 +19,19 @@ namespace Recuperatorio.Models
         [Required, StringLength(100)]
         public string Notes { get; set; } = string.Empty;
     }
+    public record CreateTicketDto
+    {
+        public Guid GuestId { get; set; }
+        public Guid EventId { get; set; }
+
+        [Required, StringLength(100)]
+        public string Type { get; set; } = string.Empty; // general | vip | backstage
+
+        public double Price { get; set; }
+
+        [Required, StringLength(100)]
+        public string Status { get; set; } = string.Empty; // valid | used | canceled
+        [Required, StringLength(100)]
+        public string Notes { get; set; } = string.Empty;
+    }
 }
