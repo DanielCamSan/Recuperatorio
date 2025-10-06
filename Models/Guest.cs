3201cfae0314534eb475ec4f462a5a4bf0ff1e26
unknown <yessicarado@gmail.com>
2025-10-06 16:36:15 -0400

    feat: adding model guest

---
+++ Models/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recuperatorio.Models
+{
+    public class Guest
+    {
+        public Guid Id { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required, StringLength(7-20)]
+        public string Phone { get; set; } = string.Empty;
+        public bool Confirmed { get; set; }=true;
+    }
+}
