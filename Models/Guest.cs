e6411c2f98c20c5dae2ab74a0ef5caa10f1f985f
unknown <yessicarado@gmail.com>
2025-10-06 17:12:30 -0400

    feat: addingdto createguestdto

--- Models/Guest.cs
+++ Models/Guest.cs
@@ -13,4 +13,14 @@ namespace Recuperatorio.Models
         public string Phone { get; set; } = string.Empty;
         public bool Confirmed { get; set; }=true;
     }
+    public record CreateGuestDto
+    {
+
+        public string FullName { get; init; } = string.Empty;
+        [Required]
+        public string Email { get; init; } = string.Empty;
+        [Required, StringLength(7 - 20)]
+        public string Phone { get; init; } = string.Empty;
+        public bool Confirmed { get; init; } = true;
+    }
 }
