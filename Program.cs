1f119dedcda9cda53424d7ae95e4686612c22a6e
unknown <yessicarado@gmail.com>
2025-10-06 16:55:35 -0400

    feat: adding rate limmit to program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.RateLimiting;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("default", config =>
+    {
+        config.PermitLimit =3;
+        config.Window = TimeSpan.FromMinutes(1);
+        config.QueueLimit = 0;
+
+    });
+});
 
 var app = builder.Build();
 
