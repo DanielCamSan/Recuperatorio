c861b717f5f11ebcd1abec2f353d5d26b10f2d2e
unknown <ernes777rim@gmail.com>
2025-10-06 16:43:20 -0400

    Feat: Added the tickets model with its corresponding attributes

---
+++ Models/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recuperatorio.Models
+{
+    public class Ticket
+    {
+
+        Guid Id {  get; set; }
+        Guid GuestId { get; set; }
+        Guid EventId { get; set; }
+        [Required, StringLength(100)]
+        string Type { get; set; } // general | vip | backstage
+        [Required, StringLength(100)]
+        float Price { get; set; }
+        [Required, StringLength(100)]
+        string Status { get; set; } // valid | used | canceled
+        [Required, StringLength(100)]
+        string? Notes { get; set; }
+    }
+}
